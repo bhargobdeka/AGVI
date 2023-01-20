68c758f7dc6adae1738e1ce8260985b4e971ebbf
bhargobdeka <69599126+bhargob-deka@users.noreply.github.com>
2023-01-20 10:34:36 -0500

    Updated RMSE code

--- RMSE_AGVI_AKF.m
+++ RMSE_AGVI_AKF.m
@@ -5,7 +5,7 @@ clear;clc
 rand_seed=4;
 RandStream.setGlobalStream(RandStream('mt19937ar','seed',rand_seed));  %Initialize random stream number based on clock
 format short
-True    = [1;-0.5;-0.3;2;0.95;4];
+True_var    = [1;-0.5;-0.3;2;0.95;4];
 folder  = {'AGVI','AKF','AKF','SWVBAKF'};
 methods = {'AGVI','ICM','CMM','SWVBAKF'};
 M       = length(methods);
@@ -14,22 +14,38 @@ n_x     = 3;                    % no. of hidden states
 n_w     = n_x;                  % no. of process error terms
 n_w2hat = n_x*(n_x+1)/2;        % total variance and covariance terms   
 Cov     = zeros(n_w2hat,M);
-RMSE    = zeros(n_w2hat,M);
-for i = 1:length(methods)
-    Q{i} = load(['Q_' folder{i} '_results/Q_' methods{i} '_Dataset1.mat']);
-    if strcmp(methods{i},'AGVI')
-        Q_est = Q{i,1}.Q_mat;
-    elseif strcmp(methods{i},'ICM')
-        Q_est = Q{i,1}.Q_est;
-    elseif strcmp(methods{i},'CMM')
-        Q_est = Q{i,1}.Q_est;
-    elseif strcmp(methods{i},'SWVBAKF')
-        Q_est = Q{i,1}.new_Qapriv;
+% RMSE    = zeros(n_w2hat,M);
+N = 5;
+for j = 1:N % for each synthetic datasets
+    for i = 1:length(methods)
+        Q{i} = load(['Q_' folder{i} '_results/Q_' methods{i} '_Dataset' num2str(j) '.mat']);
+        if strcmp(methods{i},'AGVI')
+            Q_est = Q{i,1}.Q_mat;
+        elseif strcmp(methods{i},'ICM')
+            Q_est = Q{i,1}.Q_est;
+        elseif strcmp(methods{i},'CMM')
+            Q_est = Q{i,1}.Q_est;
+        elseif strcmp(methods{i},'SWVBAKF')
+            Q_est = Q{i,1}.new_Qapriv;
+        end
+        ind = tril(true(size(Q_est)));
+        Cov(:,i)  = Q_est(ind);
+        
     end
-    ind = tril(true(size(Q_est)));
-    Cov(:,i)  = Q_est(ind);
-    RMSE(:,i) = sqrt(mean((True - Cov(:,i)).^2));
+    CovT = Cov'; % one row represent all variances for one method
+    % Cov for each method
+    CovT_AGVI(j,:)    = CovT(1,:);
+    CovT_ICM(j,:)     = CovT(2,:);
+    CovT_CMM(j,:)     = CovT(3,:);
+    CovT_SWVBAKF(j,:) = CovT(4,:);
 end
+for k = 1:length(True_var)
+RMSE_AGVI(1,k)      = sqrt(mean((True_var(k) - CovT_AGVI(:,k)).^2));
+RMSE_ICM(1,k)       = sqrt(mean((True_var(k) - CovT_ICM(:,k)).^2));
+RMSE_CMM(1,k)       = sqrt(mean((True_var(k) - CovT_CMM(:,k)).^2));
+RMSE_SWVBAKF(1,k)   = sqrt(mean((True_var(k) - CovT_SWVBAKF(:,k)).^2));
+end
+RMSE = [RMSE_AGVI;RMSE_ICM;RMSE_CMM;RMSE_SWVBAKF]
 
 
 
